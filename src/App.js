9d1eee34ab0f62c26a20f04c48628ab4ca6ddaff
Marek <marek-sobierajski@wp.pl>
2021-01-12 22:56:32 +0100

    text change to Marek Sobierajski

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Marek Sobierajski</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Marek Sobierajski - feature change
+          Learn React
         </a>
       </header>
     </div>
